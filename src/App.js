0f7a6fcc3209dd8a7d2de0d1f5df8124e7412e01
Prajwal Dahal <prajwal.x82@gmail.com>
2020-07-02 20:16:13 +0545

    Project Initialization

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import * as userServices from "./services/user";
+import CreateUser from "./pages/userForm";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      users: [],
+    };
+  }
+
+  getUserData() {
+    userServices.getAllUsers().then((response) => {});
+  }
+
+  getUserWithId() {
+    userServices.getUserWithId("2").then((response) => {});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <CreateUser />
+      </div>
+    );
+  }
 }
 
 export default App;
